2c80b15404b50d6eff969065c484806681bbfd90
Симонов Сергей Вячеславович <SVySimonov@sberbank.ru>
2021-05-13 11:04:14 +0300

    add and init h2 database

---
+++ src/main/resources/db/populate.sql
@@ -0,0 +1,6 @@
+TRUNCATE TABLE city;
+
+INSERT INTO city(NAME, REGION, DISTRICT, POPULATION, FOUNDATION)
+VALUES ('Адыгейск', 'Адыгея', 'Южный', 12248, 1973),
+       ('Майкоп', 'Адыгея', 'Южный', 144246, 1857),
+       ('Горно-Алтайск', 'Алтай', 'Сибирский', 56928, 1830);
